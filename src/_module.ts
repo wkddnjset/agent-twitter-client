8e3a99c4b42947cb9c6b5e7ec015c4f796773d44
Jangwon <wkddnjset@naver.com>
2025-02-01 00:43:03 +0900

    fix error

--- src/_module.ts
+++ src/_module.ts
@@ -4,10 +4,10 @@ export { SearchMode } from './search';
 export type { QueryProfilesResponse, QueryTweetsResponse } from './timeline-v1';
 export type { Tweet } from './tweets';
 
-export { Space } from './spaces/core/Space'
-export { SttTtsPlugin } from './spaces/plugins/SttTtsPlugin'
-export { RecordToDiskPlugin } from './spaces/plugins/RecordToDiskPlugin'
-export { MonitorAudioPlugin } from './spaces/plugins/MonitorAudioPlugin'
-export { IdleMonitorPlugin } from './spaces/plugins/IdleMonitorPlugin'
+export { Space } from './spaces/core/Space';
+export { SttTtsPlugin } from './spaces/plugins/SttTtsPlugin';
+export { RecordToDiskPlugin } from './spaces/plugins/RecordToDiskPlugin';
+export { MonitorAudioPlugin } from './spaces/plugins/MonitorAudioPlugin';
+export { IdleMonitorPlugin } from './spaces/plugins/IdleMonitorPlugin';
 
-export * from './types/spaces'
+export * from './types/spaces';
